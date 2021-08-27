ee853a81a4d0db39d58943e6acdb620dd9327dfb
frijke1978 <frijke1978@outlook.com>
2021-08-27 14:16:35 +0200

    deploy contract with default message

--- inbox/test/inbox.test.js
+++ inbox/test/inbox.test.js
@@ -1,33 +1,30 @@
-const assert = require('assert');
-const ganache = require('ganache-cli');
-const Web3 = require('web3'); //constructor
-const web3 = new Web3(ganache.provider()); //instance
+const assert = require("assert");
+const ganache = require("ganache-cli");
+const Web3 = require("web3");
+const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require('../compile');
 
+let accounts;
+let inbox;
 
-class Car {
-    park() {
-        return 'stopped'
-    }
+beforeEach(async () => {
+  // Get a list of all accounts
+    accounts = await web3.eth.getAccounts();
+    
+    // Use one of those accounts to deploy the contract
 
-    drive() {
-        return 'vroom'
-    }
-}
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['Hi there!'] })
+        .send({ from: accounts[0], gas: '1000000' })
+});
 
-let car;
+describe("Inbox", () => {
+  it("deploys a contract", () => {
+    assert.ok(inbox.options.address);
+  });
 
-beforeEach(() => {
-    car = new Car();
-})
-
-describe('Car', () => {
-    it('can park', () => {
-        // const car = new Car();
-        assert.equal(car.park(), 'stopped');
-    });
-
-    it('can drive', () => {
-        // const car = new Car();
-        assert.equal(car.drive(), 'vroom');
-    });
+  it('has a default message', async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'Hi there!');
+  });
 });
