928d7036c430b01dd5b9925a74b123ee405db660
frijke1978 <frijke1978@outlook.com>
2021-08-27 16:29:07 +0200

    deploy Contract to RinkebyTest

--- inbox/test/inbox.test.js
+++ inbox/test/inbox.test.js
@@ -1,7 +1,7 @@
 const assert = require("assert");
 const ganache = require("ganache-cli");
-const Web3 = require("web3");
-const web3 = new Web3(ganache.provider());
+const Web3 = require("web3"); // constructor
+const web3 = new Web3(ganache.provider()); // instance
 const { interface, bytecode } = require('../compile');
 
 let accounts;
@@ -14,8 +14,8 @@ beforeEach(async () => {
     // Use one of those accounts to deploy the contract
 
     inbox = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({ data: bytecode, arguments: ['Hi there!'] })
-        .send({ from: accounts[0], gas: '1000000' })
+      .deploy({ data: bytecode, arguments: ['Hi there!'] })
+      .send({ from: accounts[0], gas: '1000000' })
 });
 
 describe("Inbox", () => {
@@ -26,5 +26,11 @@ describe("Inbox", () => {
   it('has a default message', async () => {
     const message = await inbox.methods.message().call();
     assert.equal(message, 'Hi there!');
+  });
+
+  it('can change a message', async () => {
+    await inbox.methods.setMessage('bye').send({ from: accounts[0] });
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'bye');
   });
 });
