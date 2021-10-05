2099224fc39db7338d1192e05190a9c4f8d41117
frijke1978 <frijke1978@outlook.com>
2021-10-05 14:01:03 +0200

    added getSummery function

--- Campaign/ethereum/factory.js
+++ Campaign/ethereum/factory.js
@@ -1,9 +1,9 @@
-import web3 from './web3';
-import CampaignFactory from './build/CampaignFactory.json';
+import web3 from "./web3";
+import CampaignFactory from "./build/CampaignFactory.json";
 
 const instance = new web3.eth.Contract(
-    JSON.parse(CampaignFactory.interface),
-    '0x865f7E6D135c0B9719816e7f97eC6dF9E7E0c0c3'
+  JSON.parse(CampaignFactory.interface),
+  "0x6E2628E01095Ae36988F838E49Cbd49bbcB4d4fd"
 );
 
 export default instance;
