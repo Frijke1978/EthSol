e2198f63507f655548d6f0a036342ac8c514b554
frijke1978 <frijke1978@outlook.com>
2021-10-27 17:01:12 +0200

    Validate Transaction Chain

--- build_blockchain_crypto/cryptochain/blockchain/index.test.js
+++ build_blockchain_crypto/cryptochain/blockchain/index.test.js
@@ -148,6 +148,20 @@ describe('Blockchain', () => {
                 });
             });
         });
+
+        describe('and the validateTransactions flag is true', () => {
+            it('calls validTransactionData', () => {
+                const validTransactionDataMock = jest.fn();
+
+                blockchain.validTransactionData = validTransactionDataMock;
+
+                newChain.addBlock({ data: 'foo' });
+                blockchain.replaceChain(newChain.chain, true);
+
+                expect(validTransactionDataMock).toHaveBeenCalled();
+            });
+        });
+        
     });
 
     describe('validTransactionData()', () => {
@@ -227,7 +241,14 @@ describe('Blockchain', () => {
         });
         
         describe('and a block contains multiple identical transaction', () => {
-            it('returns false and logs an error', () => { });
+            it('returns false and logs an error', () => {
+                newChain.addBlock({
+                    data: [transaction, transaction, transaction, rewardTransaction]
+                });
+                
+                expect(blockchain.validTransactionData({ chain: newChain.chain })).toBe(false)
+                expect(errorMock).toHaveBeenCalled();        
+             });
         });
     });
 });
