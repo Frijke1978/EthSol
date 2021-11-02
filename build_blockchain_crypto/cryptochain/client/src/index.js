76441489f7b3546deab76000e9337ead3157bc00
frijke1978 <frijke1978@outlook.com>
2021-11-02 13:59:26 +0100

    Conduct Transaction Component

--- build_blockchain_crypto/cryptochain/client/src/index.js
+++ build_blockchain_crypto/cryptochain/client/src/index.js
@@ -5,12 +5,14 @@ import history from './history';
 import App from './components/App';
 import './index.css';
 import Blocks from './components/Blocks';
+import ConductTransaction from './components/ConductTransaction';
 
 render(
     <Router history={history}>
         <Switch>
             <Route exact exact path='/' component={App} />
             <Route path='/blocks' component={Blocks} />
+            <Route path='/conduct-transaction' component={ConductTransaction} />
         </Switch>
     </Router>,
     document.getElementById('root')
