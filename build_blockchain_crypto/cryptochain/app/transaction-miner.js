d25c083af3904f1711dc0ac8ecabda43375bf40f
frijke1978 <frijke1978@outlook.com>
2021-11-03 13:05:17 +0100

    Mine a block of transactions through the frontend

--- build_blockchain_crypto/cryptochain/app/transaction-miner.js
+++ build_blockchain_crypto/cryptochain/app/transaction-miner.js
@@ -1,32 +1,26 @@
 const Transaction = require('../wallet/transaction');
 
-
 class TransactionMiner {
-    constructor({ blockchain, transactionPool, wallet, pubsub }) {
-        this.blockchain = blockchain;
-        this.transactionPool = transactionPool;
-        this.wallet = wallet;
-        this.pubsub = pubsub;
-    }
-    
-    mineTransactions() {
-        // get the transactions pool's valid transactions
-        const validTransactions = this.transactionPool.validTransactions();
+  constructor({ blockchain, transactionPool, wallet, pubsub }) {
+    this.blockchain = blockchain;
+    this.transactionPool = transactionPool;
+    this.wallet = wallet;
+    this.pubsub = pubsub;
+  }
 
-        // generate the miner's reward
-        validTransactions.push(
-            Transaction.rewardTransaction({ minerWallet: this.wallet })
-          );
+  mineTransactions() {
+    const validTransactions = this.transactionPool.validTransactions();
 
-        // add a block consisting of these transactions to the blockchain
-        this.blockchain.addBlock({ data: validTransactions });
+    validTransactions.push(
+      Transaction.rewardTransaction({ minerWallet: this.wallet })
+    );
 
-        // broadcast the updated blockchain
-        this.pubsub.broadcastChain();
+    this.blockchain.addBlock({ data: validTransactions });
 
-        // clear the pool
-        this.transactionPool.clear();
-    }
+    this.pubsub.broadcastChain();
+
+    this.transactionPool.clear();
+  }
 }
 
 module.exports = TransactionMiner;
--- build_blockchain_crypto/cryptochain/client/src/components/Blocks.js
+++ build_blockchain_crypto/cryptochain/client/src/components/Blocks.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Button } from 'react-bootstrap'
 import { Link } from 'react-router-dom';
 import Block from './Block';
 
@@ -6,7 +7,7 @@ class Blocks extends Component {
     state = { blocks: [] };
     
     componentDidMount() {
-        fetch(`${document.location.origin}api/blocks`)
+        fetch(`${document.location.origin}/api/blocks`)
             .then(response => response.json())
             .then(json => this.setState({ blocks: json }));
     }
